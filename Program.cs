33a1e951b24bbc5e9b429db7eb281ef0471e1e57
Maria <bekkerm.home@gmail.com>
2022-12-21 23:09:01 +0300

    Решена задача 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int Test ()
+// {
+// int num = Convert.ToInt32(Console.ReadLine());
+// int hunt = num / 100;
+// int ten = num / 10 % 10;
+// int one = num % 10;
+// return (ten);
+// }
+// Console.WriteLine("Введите трехзначное число: ");
+// int res = Test();
+// Console.WriteLine(res);
