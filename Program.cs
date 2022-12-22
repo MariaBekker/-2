8f3da2c7808fc648067137a8faaa9eaa4b824e30
Maria <bekkerm.home@gmail.com>
2022-12-23 00:09:48 +0300

    Решено 2,5 задачи из 3

--- Program.cs
+++ Program.cs
@@ -11,3 +11,47 @@
 // Console.WriteLine("Введите трехзначное число: ");
 // int res = Test();
 // Console.WriteLine(res);
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// void Test ()
+// {
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num > 100)
+// {
+// int one = num % 10;
+// Console.WriteLine(one);
+// }
+// else 
+// {
+// Console.WriteLine("Третьей цифры нет");
+// }
+// }
+// Console.WriteLine("Введите число: ");
+// Test();
+
+
+//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Test ()
+{
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 5)
+{
+Console.WriteLine("Выходной день");
+}
+else 
+{
+Console.WriteLine("Рабочий день");
+}
+}
+Console.WriteLine("Введите день недели: ");
+Test();
+
+
+
+
+
+
+
